605d6451e2910f54a38cca4a63fc88a3cc34b0da
Ryan Reynolds <reynoldsryanscott@gmail.com>
2016-01-25 19:52:59 -0800

    [debug] navbar routing state updating now

--- client/index.js
+++ client/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { Router, Route } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistory } from 'react-router-redux';
 import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
@@ -10,16 +11,18 @@ import Profile from './components/profile';
 import MyPlants from './components/myplants';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = compose(applyMiddleware(ReduxPromise), window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore);
+const history = browserHistory;
+const middleware = syncHistory(history);
+const createStoreWithMiddleware = compose(applyMiddleware(middleware, ReduxPromise), window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore);
+const store = createStoreWithMiddleware(reducers);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <div>
-      <Router>
+      <Router history={history}>
         <Route path='/' component={App}>
-          <Route path='/myplants' component={MyPlants}>
-            <Route path='/profile' component={Profile} />
-          </Route>
+          <Route path='myplants' component={MyPlants} />
+          <Route path='profile' component={Profile} />
         </Route>
       </Router>
     </div>
