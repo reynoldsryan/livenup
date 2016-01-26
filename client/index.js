e09f439920f5b7bb65c53486e4faadcf020cc184
Ryan Reynolds <reynoldsryanscott@gmail.com>
2016-01-25 19:35:10 -0800

    [feature] navbar switching views but routing state is not updating

--- client/index.js
+++ client/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { Router, Route } from 'react-router';
 import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
+import Profile from './components/profile';
+import MyPlants from './components/myplants';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = compose(applyMiddleware(ReduxPromise), window.devtoolsExtension ? window.devtoolsExtension() : f => f)(createStore);
+const createStoreWithMiddleware = compose(applyMiddleware(ReduxPromise), window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <div>
+      <Router>
+        <Route path='/' component={App}>
+          <Route path='/myplants' component={MyPlants}>
+            <Route path='/profile' component={Profile} />
+          </Route>
+        </Route>
+      </Router>
+    </div>
   </Provider>, document.querySelector('.container-fluid'));
