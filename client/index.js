6db1ede1acdebb22cc22b91aa05b518a14e0daf2
Ryan Reynolds <reynoldsryanscott@gmail.com>
2016-01-25 17:00:39 -0800

    navbar created - not functional yet

---
+++ client/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import ReduxPromise from 'redux-promise';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = compose(applyMiddleware(ReduxPromise), window.devtoolsExtension ? window.devtoolsExtension() : f => f)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>, document.querySelector('.container-fluid'));
