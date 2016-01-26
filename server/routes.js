2963a437814de63440680268e3982d4a286e36a5
Genevieve <home@Genevieve-Kolves-MacBook-Pro.local>
2016-01-25 19:28:07 -0800

    [feature] scafolds authentication

--- server/routes.js
+++ server/routes.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/login', (req, res) => {
-  //call a function in auth.js
+  //call a function in auth.js passing in req and res
   //verify user in database and create a token
     //here after use middleware to verify token
 });
 
-module.exports = routes;
+module.exports = router;
