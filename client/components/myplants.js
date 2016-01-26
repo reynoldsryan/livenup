7a40550a13403c7f3ea4550ba2d2b39e39dc1edf
Erik McGarraugh <erikmcgarraugh@gmail.com>
2016-01-26 15:39:42 -0800

    [feature] basic add plot functionality

--- client/components/myplants.js
+++ client/components/myplants.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
+import Planner from '../containers/planner';
 
 export default class Profile extends Component {
   render() {
     return (
-      <div>These are my plants.
-      {this.props.children}</div>
+      <Planner />
     );
   }
 }
---
+++ client/containers/planner.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { fetchUserPlots, addPlot } from '../actions/index';
+
+import Plot from './plot';
+
+class Planner extends Component {
+  constructor(props) {
+    super(props);
+    console.log('props in planner', props);
+    this.state = {
+      counter: 1
+    };
+    this.handleAddPlot = this.handleAddPlot.bind(this);
+    this.renderPlots = this.renderPlots.bind(this);
+  }
+
+  componentWillMount() {
+    this.props.fetchUserPlots();
+    console.log('component will mount');
+  }
+
+  renderPlots(plot) {
+    console.warn('Rendering plots');
+    console.log('plot in planner',plot)
+    return <Plot plot={plot} />;
+  }
+
+  handleAddPlot(e) {
+    this.props.addPlot({
+      name: `Plot${this.state.counter}`,
+      length: 4,
+      width: 2,
+      plants: []
+    });
+    this.setState({counter: this.state.counter + 1});
+    console.log('add plot clicked');
+  }
+
+  render() {
+
+    return (
+      <div>
+        This is the planner view
+        <button onClick={this.handleAddPlot}>Add Plot</button>
+        {this.props.plots.map(this.renderPlots)}
+      </div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  // console.log('state in planner.js from fn()mapDispatchTopProps', dispatch);
+  return bindActionCreators({ fetchUserPlots, addPlot}, dispatch);
+}
+
+function mapStateToProps({ userPlots }) {
+  if(userPlots) {
+    return { plots: userPlots.plots };
+  }
+  // console.log('plots state', userPlots)
+  return { plots: []  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Planner);
