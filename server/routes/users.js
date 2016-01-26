34b445960eb66cc5e32bca8a91a67d5bd1e217db
Genevieve <home@Genevieve-Kolves-MacBook-Pro.local>
2016-01-26 14:53:54 -0800

    [feature] adds user specific routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,2 +1,20 @@
 import { express } from 'express';
+import { auth } from './../auth';
+
 const router = express.Router();
+
+router.route('/', auth.checkUser)
+  .post((req, res) => {
+    const userData = req.body.user.params;
+    //calls database and saves user profile
+  })
+  .put((req, res) => {
+    const userData = req.body.user.params;
+    //calls database and modifies a user profile;
+  })
+  .delete((req, res) => {
+    const userName = req.body.user.username;
+    //calls a database function that deletes the specified user
+  });
+
+module.exports = router;
