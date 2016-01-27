1458068702285c99997a2752d627d326e712f3a7
Ryan Reynolds <reynoldsryanscott@gmail.com>
2016-01-26 19:07:54 -0800

    jwt auth in progress

---
+++ client/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+import MyPlants from './components/myplants';
+import Profile from './components/profile';
+import Login from './components/login';
+import Signup from './components/signup';
+
+
+export default (
+<Route path='/' component={App}>
+  <Route path='myplants' component={MyPlants} />
+  <Route path='profile' component={Profile} />
+  <Route path='login' component={Login} />
+  <Route path='signup' component={Signup} />
+</Route>
+);
