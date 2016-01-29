f71fc1541c8c790d67a5d20bbd7d36b21b728f21
Erik McGarraugh <erikmcgarraugh@gmail.com>
2016-01-28 16:01:03 -0800

    [feature] ability to add, remove, edit plots added

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,8 +1,11 @@
+import axios from 'axios';
+
 export const FETCH_USER_PLOTS = 'FETCH_USER_PLOTS';
+export const PLANT_SEARCH = 'PLANT_SEARCH';
 
 export const ADD_PLOT = 'ADD_PLOT';
 export const REMOVE_PLOT = 'REMOVE_PLOT';
-export const RENAME_PLOT = 'RENAME_PLOT';
+export const UPDATE_PLOT = 'UPDATE_PLOT';
 
 export const ADD_PLANT = 'ADD_PLANT';
 export const REMOVE_PLANT = 'REMOVE_PLANT';
@@ -21,43 +24,107 @@ let dummyData = {
 
 export function fetchUserPlots() {
   console.log('fetching user data');
-  const fakeRequest = new Promise(function(resolve, reject){
+  // const fakeRequest = new Promise(function(resolve, reject){
+  //   setTimeout(function () {
+  //     console.log('fetched user data!');
+  //     resolve({data: dummyData});
+  //   }, 500);
+  // });
+  const request = axios.get('/plot', {params: {user: 'oliver'}})
+                  .catch((response) => {
+                    if(response  instanceof Error) {
+                      console.error('GET | Error sending reponse',response);
+                    }else {
+                      console.log('GET | error from server', response);
+                    }
+                  });
+
+  // return {
+  //   type: FETCH_USER_PLOTS,
+  //   payload: fakeRequest
+  // }
+  return {
+    type: FETCH_USER_PLOTS,
+    payload: request
+  }
+}
+
+export function plantSearch(plant) {
+  console.log('Searching for plants matching:',plant);
+  const requestResults = new Promise(function(resolve, reject) {
     setTimeout(function () {
-      console.log('fetched user data!');
-      resolve({data: dummyData});
-    }, 500);
+      resolve(['sunflower', 'daisy', 'carrot', 'potato']);
+    }, 300);
   });
 
   return {
-    type: FETCH_USER_PLOTS,
-    payload: fakeRequest
+    type: PLANT_SEARCH,
+    payload: requestResults
   }
 }
 
 export function addPlot(newPlot) {
   console.log('calling addPlot in actions/index.js with arg', newPlot);
+  const request = axios.post('/plot', {
+    data: {
+      user: 'Oliver',
+      plot: newPlot
+    }
+  }).catch((response) => {
+    if(response  instanceof Error) {
+      console.error('POST | Error sending reponse',response);
+    }else {
+      console.log('POST | error from server', response);
+    }
+  });
+
+  // return {
+  //   type: ADD_PLOT,
+  //   payload: { newPlot }
+  // }
   return {
     type: ADD_PLOT,
-    payload: { newPlot }
+    payload: request
   }
 }
 
-export function removePlot(plotName) {
-
+export function removePlot(plotId) {
+  const request = axios.delete('/plot', {params: {id: plotId}})
+                  .catch((response) => {
+                    if(response  instanceof Error) {
+                      console.error('DELETE | Error sending reponse',response);
+                    }else {
+                      console.log('DELETE | error from server', response);
+                    }
+                  });
+  // return {
+  //   type: REMOVE_PLOT,
+  //   payload: { plotId }
+  // }
   return {
     type: REMOVE_PLOT,
-    payload: { plotName }
+    payload: request
   }
 }
 
-export function renamePlot(targetPlot, newPlotName) {
+export function updatePlot(updatedPlot) {
+  console.log('TRYING TO UPDATE PLOT',updatePlot);
+  const request = axios.put('/plot', {
+                    data: {
+                      plot: updatedPlot
+                    }
+                  })
+                  .catch((response) => {
+                    if(response  instanceof Error) {
+                      console.error('DELETE | Error sending reponse',response);
+                    }else {
+                      console.log('DELETE | error from server', response);
+                    }
+                  });
 
   return {
-    type: RENAME_PLOT,
-    payload: {
-      targetPlot: targetPlot,
-      newPlotName: newPlotName
-    }
+    type: UPDATE_PLOT,
+    payload: request
   }
 }
 
--- client/reducers/reducer_plots.js
+++ client/reducers/reducer_plots.js
@@ -1,50 +1,73 @@
 import { FETCH_USER_PLOTS } from '../actions/index';
 
-import { ADD_PLOT, REMOVE_PLOT, RENAME_PLOT } from '../actions/index';
+import { ADD_PLOT, REMOVE_PLOT, UPDATE_PLOT } from '../actions/index';
 import { ADD_PLANT, REMOVE_PLANT } from '../actions/index';
 
 export default function(state = null, action) {
   console.log('Redcer state:',state,'action',action);
   switch (action.type) {
     case FETCH_USER_PLOTS:
-      console.log('Fetch user plots, payload:', action.paylaod);
-      return action.payload.data;
+      console.log('Fetch user plots, payload:', action.payload);
+      return {
+        plots: action.payload.data,
+        plants: []
+      };
       break;
     case ADD_PLOT:
       console.log('Add plot, payload:', action.payload);
+      if(action.payload.data) {
+        return {
+          plants: [...state.plants],
+          plots: [...state.plots, action.payload.data.data]
+        };
+      }else {
       return {
-        plants: [...state.plants],
-        plots: [...state.plots, action.payload.newPlot]
-      };
+          plants: [...state.plants],
+          plots: [...state.plots]
+        };
+      }
       break;
     case REMOVE_PLOT:
       console.log('Remove plot, payload:', action.payload);
       let plotIndex = null;
-      state.plots.forEach((plot, i) => {
-        console.error('plot',plot);
-        if(plot.name === action.payload.plotName) {
-          plotIndex = i;
-        }
-      });
+      if(action.payload.data.data.message === "Successfully deleted plot") {
+        state.plots.forEach((plot, i) => {
+          console.error('plot',plot);
+          if(plot._id === action.payload.data.data._id) {
+            plotIndex = i;
+          }
+        });
+      }
+      // else {
+      //   console.error('Error deleting plot id: ', action.payload.plotId);
+      // }
       let plots = state.plots.slice();
       console.log('index of plot', plotIndex);
-      plots.splice(plotIndex, 1);
-      return {
-        plants: state.plants.slice(),
-        plots: plots
-      };
+      if(plotIndex >= 0) {
+        plots.splice(plotIndex, 1)
+        return {
+          plants: state.plants.slice(),
+          plots: plots
+        };
+      }
+      return state;
       break;
 
-    case RENAME_PLOT:
-      console.log('Renaming plot, payload: ',action.payload);
-      let updatedPlots = state.plots.slice();
-      updatedPlots.forEach((v) => {
-        if(v.name === action.payload.targetPlot) {
-          const newName = action.payload.newPlotName;
-          console.log('Renaiming plot:',v.name,'to', newName);
-          v.name = newName;
-        }
-      });
+    case UPDATE_PLOT:
+      console.log('Updating plot plot, payload: ',action.payload);
+      let updatedPlots = state.plots.slice();;
+
+      if(action.payload.data.message === "Successfully updated the plot") {
+
+        state.plots.forEach((plot, i) => {
+          console.error('plot',plot);
+          if(plot._id === action.payload.data.data._id) {
+            console.log('plot._id === action.payload.data.data._id',plot._id === action.payload.data.data._id);
+            updatedPlots[i] = action.payload.data.data;
+          }
+        });
+      }
+      console.log('Updated Plots', updatedPlots);
       return {
         plants: state.plants.slice(),
         plots: updatedPlots
