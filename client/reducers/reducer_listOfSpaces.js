a579643f2b6971c67028a4a2d8d8c6ff20cb3025
Ryan Reynolds <reynoldsryanscott@gmail.com>
2016-02-01 17:49:27 -0800

    [feature] adds redirect to login for protected page

--- client/reducers/reducer_listOfSpaces.js
+++ client/reducers/reducer_listOfSpaces.js
@@ -6,7 +6,7 @@ export default function () {
     {title: 'Kitchen', img: 'http://interiordesignshome.com/wp-content/uploads/2015/02/Plants-on-the-kitchen-walls.jpg'},
     {title: 'Front Yard', img: 'http://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2010/2/22/0/front-yard-rms_cottage-style-garden-ebboy-05_s4x3.jpg.rend.hgtvcom.1280.960.jpeg'},
     {title: 'Back Yard', img: 'http://www.thefitzpatrickgroup.com/agent_files/Used/natural-backyard-landscaping-ideas-for-kids.jpg'},
-    {title: 'Garden', img: 'http://lemerg.com/data/wallpapers/19/932730.jpg'},
+    {title: 'Office', img: 'http://interfacedesignspace.com/wp-content/uploads/2015/03/plant2.jpg'},
     {title: 'Patio', img: 'http://st.hzcdn.com/simgs/4d5107500f9ad06c_4-3571/mediterranean-patio.jpg'},
     {title: 'Balcony', img: 'http://2.lushome.com/wp-content/uploads/2012/04/balcony-decorating-flowers-plants-18.jpg'}
   ];
