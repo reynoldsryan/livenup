907883489b0ec84c8d62f24af232cdc282c79ed2
jmichelin <jmichelinphone@gmail.com>
2016-02-01 17:29:17 -0800

    [feature] Displays inspirational images

--- client/components/app.js
+++ client/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import NavBar from './navbar';
+import Inspirations from '../containers/inspirations'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <Inspirations />
         {this.props.children}
       </div>
     );
