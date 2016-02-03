1dc441ad3ba60f5748ccd03ff4186f1e33dde5bb
jmichelin <jmichelinphone@gmail.com>
2016-02-03 10:24:30 -0800

    [chore] removes merge conflicts from routes.js

--- client/routes.js
+++ client/routes.js
@@ -4,11 +4,8 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import Index from './components/index';
 import MySpaces from './components/myspaces';
-<<<<<<< 067d60766df211b433ebcba251dfacea048e56d4
-=======
 import Login from './containers/login';
 import Signup from './containers/signup';
->>>>>>> [refactor] adds inspirations to routes.js
 import Inspirations from './containers/inspirations';
 
 const checkAuth = (nextState, replace) => {
@@ -22,11 +19,8 @@ export default (
   <IndexRoute component={Index} />
   <Route path='inspirations' component={Inspirations} />
   <Route path='mygreenspace' onEnter={checkAuth} component={MySpaces} />
-<<<<<<< 067d60766df211b433ebcba251dfacea048e56d4
-=======
   <Route path='login' component={Login} />
   <Route path='signup' component={Signup} />
   <Route path='inspirations' component={Inspirations} />
->>>>>>> [refactor] adds inspirations to routes.js
 </Route>
 );
