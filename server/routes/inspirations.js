6debc7553d2c47a48fb289965f86582b87b5791a
Genevieve <home@Genevieve-Kolves-MacBook-Pro.local>
2016-02-02 17:23:52 -0800

    [feature] adds inspirations routing

--- server/routes/inspirations.js
+++ server/routes/inspirations.js
@@ -2,3 +2,20 @@
 
 const express = require('express');
 const inspire = require('../../database/inspirations');
+const router = express.Router();
+
+router.route('/')
+  .get((req, res) =>  {
+    inspire.getRooms(function(data){
+      res.send(data);
+    });
+  })
+  .post((req, res) => {
+    let _category = req.body.inspirations.category;
+
+    inspire.getImages({category: _category}, function(data){
+      res.send(data);
+    });
+  })
+
+module.exports = router;
