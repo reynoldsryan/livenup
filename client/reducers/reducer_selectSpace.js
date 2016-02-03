15ad9f8ffb7d6dad5a00290376f2a942a0b53566
Ryan Reynolds <reynoldsryanscott@gmail.com>
2016-02-02 19:50:12 -0800

    [feature] links main page tiles to inspiration page

--- client/reducers/reducer_selectSpace.js
+++ client/reducers/reducer_selectSpace.js
@@ -1,7 +1,6 @@
 import { SELECT_SPACE } from '../actions/index';
 
 export default function(state = null, action) {
-  console.log(action);
   switch(action.type) {
     case SELECT_SPACE:
       return action.payload;
