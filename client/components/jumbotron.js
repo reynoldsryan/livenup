d7e25c7c9c0b710608866ada450e7efc95bb21d3
Ryan Reynolds <reynoldsryanscott@gmail.com>
2016-02-03 15:09:05 -0800

    [refactor] deleted bootstrap navbar component

--- client/components/jumbotron.js
+++ client/components/jumbotron.js
@@ -3,13 +3,11 @@ import React, { Component } from 'react';
 export default class VideoJumbotron extends Component {
   render() {
     return(
-      <div className='header-container'>
         <div className='video-container'>
           <video autoPlay loop volume='0' poster=''>
             <source src='../../assets/Greenhouse.mp4' type='video/mp4' />
           </video>
         </div>
-      </div>
     );
   }
 }
