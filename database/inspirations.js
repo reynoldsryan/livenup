392d9b7f2b7122413c24014aa887dc63dc6fd9d1
jmichelin <jmichelinphone@gmail.com>
2016-02-03 12:12:25 -0800

    [feature] adds correct formatting to POST request at /inspirations

--- database/inspirations.js
+++ database/inspirations.js
@@ -2,35 +2,35 @@
 const mongoose = require('mongoose');
 
 const inspireRoomsSchema = new mongoose.Schema( {
-  name: {type: String},
-  light: {type: String},
-  humidity: {type: String},
-  temperature: {type: String}
+name: {type: String},
+light: {type: String},
+humidity: {type: String},
+temperature: {type: String}
 });
 
 const Rooms = mongoose.model('spaces', inspireRoomsSchema)
 
 const inspireImagesSchema = new mongoose.Schema( {
-  inspiration_name: {type: String},
-  category: {type: String},
-  image_url: {type: String},
-  plants: {type: Array}
-})
+inspiration_name: {type: String},
+category: {type: String},
+image_url: {type: String},
+plants: {type: Array}
+});
 
 const Images = mongoose.model('space_inspirations', inspireImagesSchema)
 
 module.exports = {
-  getRooms (callback) {
-    Rooms.find({}, (err, result) => {
-      if(err) console.error(err);
-      callback(result);
-    });
-  },
+getRooms (callback) {
+  Rooms.find({}, (err, result) => {
+    if(err) console.error(err);
+    callback(result);
+  });
+},
 
-  getImages (category, callback) {
-    Images.find({category: category}, (err, result) => {
-      if(err) console.error(err);
-      callback(result);
-    });
-  }
+getImages (category, callback) {
+  Images.find({category: category}, (err, result) => {
+    if(err) console.error(err);
+    callback(result);
+  });
+}
 }
