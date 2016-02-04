398b7f23737e0b9c53f232797021236971ba2d05
Genevieve <home@Genevieve-Kolves-MacBook-Pro.local>
2016-02-03 16:24:38 -0800

    [refactor] updates space schema

--- server/routes/spaces.js
+++ server/routes/spaces.js
@@ -4,43 +4,49 @@ const express = require('express');
 const auth = require('./../auth');
 const space = require('../../database/spaces');
 const plant = require('../../database/plants');
+const jwt = require('jwt-simple');
 
 const router = express.Router();
 
 router.route('/')
   .get(auth.checkUser, (req, res) => {
-    console.log('ln 9: in route/plots GET');
-
     let _user = req.query.user;
-
     space.find(_user, function(data){
       res.send(data);
     });
   })
   .post(auth.checkUser, (req, res) => {
-    console.log('ln 17: in route/spaces POST');
+    let _token = req.headers.token;
+    let _decoded = jwt.decode(_token, secret.salt);
 
-    let _name = req.body.space.name;
-    let _length = req.body.space.length;
-    let _width = req.body.space.width;
+    let _space_name = req.body.space._space_name;
+    let _space_useremail = _decoded.email;
+    let _space_image = req.body.space.space_image;
+    let _category = req.body.space.category;
+    let _light = req.body.space.light;
+    let _humidity = req.body.space.humidity;
+    let _temperature = req.body.space.temperature;
     let _plants = req.body.space.plants;
-    let _user = req.body.user.email;
 
-    space.add(_name, _length, _width, _plants, _user, function(data){
+    space.add([_space_name, _space_useremail, _space_image, _category, _light, _humidity, _temperature, _plants], function(data){
       res.send(data);
     });
   })
   .put(auth.checkUser, (req, res) => {
-    console.log('ln 28: in route/spaces PUT');
+    let _token = req.headers.token;
+    let _decoded = jwt.decode(_token, secret.salt);
 
-    let _space = req.body.space.name;
-    let _length = req.body.space.length;
-    let _width = req.body.space.width;
+    let _id = req.body.space.id;
+    let _space_name = req.body.space._space_name;
+    let _space_useremail = _decoded.email;
+    let _space_image = req.body.space.space_image;
+    let _category = req.body.space.category;
+    let _light = req.body.space.light;
+    let _humidity = req.body.space.humidity;
+    let _temperature = req.body.space.temperature;
     let _plants = req.body.space.plants;
-    let _user = req.body.user;
-    let _id = req.body.space._id;
 
-    space.update(_id, [_space, _length, _width, _plants, _user], function(data) {
+    space.update(_id, [_space_name, _space_useremail, _space_image, _category, _light, _humidity, _temperature, _plants], function(data) {
       res.send(data);
     });
   })
@@ -53,32 +59,6 @@ router.route('/')
     });
   });
 
-// router.route('/plant')
-//   .get(auth.checkUser, (req, res) => {
-//     let _plant = req.query.plant;
-//     //calls database function to return info on plant;
-//     console.log('in route /spaces/plants GET');
-//
-//     plant.find(_plant, function(data))
-//   })
-//   .post(auth.checkUser, (req, res) => {
-//     let plant = req.body.plant;
-//     //calls a database function to save a plant
-//     console.log('in route /spaces/plants POST');
-//   })
-//   .put(auth.checkUser, (req, res) => {
-//     let plant = req.body.plant;
-//     //calls a database function to edit a plant
-//     console.log('in route /spaces/plants PUT');
-//   })
-//   .delete(auth.checkUser, (req, res) => {
-//     console.log('in route /spaces/plants DELETE');
-//
-//     let _id = req.body.space._id;
-//     space.remove(id, function(data) {
-//       res.send(data);
-//     });
-//   });
 
 
 module.exports = router;
