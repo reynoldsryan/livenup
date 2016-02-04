019cffe91da7f289f05d986faef8e7b3b607a0e6
Charles Meredith <charles3m@gmail.com>
2016-02-03 18:56:55 -0800

    [feature] adds three number icons and text to home page

--- client/components/howToSteps.js
+++ client/components/howToSteps.js
@@ -1,1 +1,31 @@
 import React, { Component } from 'react';
+import Paper from 'material-ui/lib/paper';
+
+export default class HowToSteps extends Component {
+  render() {
+    const style = {
+      height: 150,
+      width: 150,
+      margin: 100,
+      textAlign: 'center',
+      color: 'purple',
+      fontSize: '800%',
+      display: 'inline-block'
+    };
+    const textStyle = {
+      height: 100,
+      width: 100,
+      margin: 20,
+      textAlign: 'center',
+      fontSize: '100%',
+      display: 'inline-block',
+    };
+    return(
+      <div>
+        <Paper style={style} zDepth={1} circle={true}>1</Paper>
+        <Paper style={style} zDepth={1} circle={true}>2</Paper>
+        <Paper style={style} zDepth={1} circle={true}>3</Paper>
+      </div>
+    );
+  }
+}
