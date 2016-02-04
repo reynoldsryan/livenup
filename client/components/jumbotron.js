71d116e104841bd98efcf9f19bdb0143a4b9174c
Ryan Reynolds <reynoldsryanscott@gmail.com>
2016-02-03 20:08:59 -0800

    [refactor] added text to jumbotron

--- client/components/jumbotron.js
+++ client/components/jumbotron.js
@@ -3,11 +3,14 @@ import React, { Component } from 'react';
 export default class VideoJumbotron extends Component {
   render() {
     return(
+      <div className='header-container'>
         <div className='video-container'>
           <video autoPlay loop volume='0' poster=''>
             <source src='../../assets/flowers.mp4' type='video/mp4' />
           </video>
         </div>
+        <h3 className='jumbotron-heading'>Add life to your life.</h3>
+      </div>
     );
   }
 }
