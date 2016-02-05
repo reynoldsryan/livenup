42739eb6e62da6a231f4eac171813ab0a500b343
Erik McGarraugh <erikmcgarraugh@gmail.com>
2016-02-05 09:47:08 -0800

    [feature] user can create and fetch spaces

--- client/reducers/reducer_userSpaces.js
+++ client/reducers/reducer_userSpaces.js
@@ -1,17 +1,33 @@
 import CREATE_SPACE from '../actions/creator_actions';
 import UPDATED_USERSPACE from '../actions/creator_actions';
+import FETCH_USER_SPACES from '../actions/spaces_actions';
 
 export default function(state = [], action) {
+  console.log('action '+FETCH_USER_SPACES, action);
   switch(action.type) {
     case UPDATED_USERSPACE:
+    console.log('UPDATED_SPACE Action, ',action);
+      return state.slice();
     case CREATE_SPACE:
+    console.log('CREATE_SPACE Action, ',action);
       if(action.payload === undefined) {
         //basic error handling
         console.error('++++Payload in reducer_userSpaces for action CREATE_SPACE is undefined');
         return state.slice();
       }
-      return action.payload.data.spaces;
+
+      return [...state, action.payload.data];
+    case "FETCH_USER_SPACES":
+    console.log(FETCH_USER_SPACES, action);
+      if(action.payload === undefined) {
+        //basic error handling
+        console.error('++++Payload in reducer_userSpaces for action CREATE_SPACE is undefined');
+        return state.slice();
+      }
+
+      return action.payload.data;
 
     default: return state;
   }
+  return state;
 }
