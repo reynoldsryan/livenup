39bc25c080441c9bc69b84a1f099ac6b771c7b97
jmichelin <jmichelinphone@gmail.com>
2016-02-06 14:44:42 -0800

    [chore] fixes image url typos

--- client/reducers/reducer_listOfSpaces.js
+++ client/reducers/reducer_listOfSpaces.js
@@ -1,13 +1,13 @@
 export default function () {
   return [
-    {title: 'Bedroom', img: '../../images/bedroom.jpg'},
-    {title: 'Living Room', img: '../../images/livingroom.jpg'},
-    {title: 'Bathroom', img: '../../images/bathroom.jpg'},
-    {title: 'Kitchen', img: '../../images/kitchen.jpg'},
-    {title: 'Front Yard', img: '../../images/frontyard.jpg'},
-    {title: 'Back Yard', img: '../../images/backyard.jpg'},
-    {title: 'Office', img: '../../images/office.jpg'},
-    {title: 'Patio', img: '../../images/patio.jpg'},
-    {title: 'Balcony', img: '../../images/balcony.jpg'}
+    {title: 'Bedroom', img: '../../assets/images/bedroom.jpg'},
+    {title: 'Living Room', img: '../../assets/images/livingroom.jpg'},
+    {title: 'Bathroom', img: '../../assets/images/bathroom.jpg'},
+    {title: 'Kitchen', img: '../../assets/images/kitchen.jpg'},
+    {title: 'Front Yard', img: '../../assets/images/frontyard.jpg'},
+    {title: 'Back Yard', img: '../../assets/images/backyard.jpg'},
+    {title: 'Office', img: '../../assets/images/office.jpg'},
+    {title: 'Patio', img: '../../assets/images/patio.jpg'},
+    {title: 'Balcony', img: '../../assets/images/balcony.jpg'}
   ];
 }
